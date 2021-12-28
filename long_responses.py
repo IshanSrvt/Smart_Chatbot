0a76e47227a0389d95a4b9e42bcf4e83cb71768d
IshanSrvt <76547283+IshanSrvt@users.noreply.github.com>
2021-12-28 18:04:02 +0530

    Adding time to long resposnses

--- long_responses.py
+++ long_responses.py
@@ -1,7 +1,14 @@
 import random
+from datetime import datetime
+
+
+#------------ Finding Time --------------
+now = datetime.now()
+current_time = now.strftime("%H:%M")
 
 R_EATING = "I don't like eating anything because I'm a bot obviously!"
 R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
+R_TIME = (f"Sir, The time is {current_time}")
 
 
 def unknown():
@@ -11,3 +18,4 @@ def unknown():
                 "What does that mean?"][
         random.randrange(4)]
     return response
+
